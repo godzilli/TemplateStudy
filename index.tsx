151f8ff282aeef7b0b36b8d53faa82d03a956065
Jennifer <50343150+godzilli@users.noreply.github.com>
2022-07-01 18:10:52 -0300

    uploading src

---
+++ index.tsx
@@ -0,0 +1,13 @@
+/*IMPORTA E RENDERIZA APP.TSX */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
